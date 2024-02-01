5c4367f5b1fdb947afe63b85227d5149b01b8923
Eray Özer <erayozer91@protonmail.com>
2024-02-01 23:21:41 +0100

    log upload command

--- client/src/main/java/com/erayoezer/services/UploadService.java
+++ client/src/main/java/com/erayoezer/services/UploadService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class UploadService extends FileCommandsTemplate {
+
+    private final static Logger LOG = Logger.getLogger(UploadService.class.getName());
 
     public UploadService(Db db, ServerSocket serverSocket) {
         super(db, serverSocket);
@@ -18,10 +21,12 @@ public class UploadService extends FileCommandsTemplate {
     protected void command(String bucketLocation, Optional<String> path) {
         try {
             if (path.isEmpty()) {
+                LOG.severe("Path does not exist while upload command.");
                 throw new RuntimeException("path should exist"); // TODO custom exception
             }
             serverSocket.sendFile(path.get());
         } catch (IOException e) {
+            LOG.severe("Error while running upload command.");
             throw new RuntimeException(e);
         }
     }
